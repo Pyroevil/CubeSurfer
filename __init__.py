f56072a4bc8aca584ecb7a03b9d282a9aa13109e
JF <pyroevil@hotmail.com>
2015-03-30 21:22:06 -0400

    uv transfert from emitter

--- __init__.py
+++ __init__.py
@@ -39,6 +39,7 @@ import sys
 from cubesurfer import mciso
 import time
 import ctypes
+from mathutils.geometry import barycentric_transform as barycentric
 
 tmframe = 'init'
 
@@ -114,7 +115,7 @@ def isosurf(context):
     
     stime = time.clock()
     global a
-    
+    print("Start calculation of isosurface...frame:",bpy.context.scene.frame_current)  
     SurfList = []
     i = 0
     for object in bpy.context.scene.objects:
@@ -132,18 +133,18 @@ def isosurf(context):
             i += 1
                             
     for surfobj in SurfList:
-        print("Start calculation of isosurface...frame:",bpy.context.scene.frame_current)
         #print(bpy.context)
         #print(bpy.context.screen)
         #print(bpy.context.window)
         #print(bpy.context.area)
         #print(surfobj)
         obsurf,mesurf,res,preview = surfobj[0]
+        print("  ----Surf Object:",obsurf.name,"----")
         #print(obsurf,mesurf)
         #print(surfobj[1][0])
         ploc = []
         psize = []
-        pvel = []
+        pprop = []
         stime = time.clock()
         xmin = 10000000000
         xmax = -10000000000
@@ -153,14 +154,23 @@ def isosurf(context):
         zmax = -10000000000
         for obj,psys,sizem in surfobj[1:]:
             #print(obj,psys,res,sizem)
-            psys = bpy.data.objects[obj].particle_systems[psys].particles
+            psys = bpy.data.objects[obj].particle_systems[psys]
             #print(psys)
-            psysize = len(psys)
+            if 'IsoLocalUV' not in psys.settings:
+                print("  WARNING: no IsoLocalUV prop found")
+                psys.settings['IsoLocalUV'] = [0,0,0]
+            psysize = len(psys.particles)
+            if len(psys.settings['IsoLocalUV']) != (psysize * 3):
+                print("  WARNING: not same numbers of props found")
+                psys.settings['IsoLocalUV'] = [0] * psysize * 3
             for par in range(psysize):
-                if psys[par].alive_state == 'ALIVE':
-                    size = psys[par].size * sizem
-                    ploc.append(psys[par].location)
-                    pvel.append(psys[par].velocity)
+                if psys.particles[par].alive_state == 'ALIVE':
+                    size = psys.particles[par].size * sizem
+                    ploc.append(psys.particles[par].location)
+                    uvx = psys.settings['IsoLocalUV'][par*3]#psys.particles[par].location.x - 
+                    uvy = psys.settings['IsoLocalUV'][par*3+1]#psys.particles[par].location.y - 
+                    uvz = psys.settings['IsoLocalUV'][par*3+2]#psys.particles[par].location.z - 
+                    pprop.append(psys.particles[par].velocity.to_tuple() + (uvx,uvy,uvz))
                     psize.append(size)
                     
         if len(psize) > 0:
@@ -168,7 +178,7 @@ def isosurf(context):
             isolevel=0.0
             print('  pack particles:',time.clock() - stime,'sec')
             
-            a,b = mciso.isosurface(res,isolevel,ploc,psize,pvel)
+            a,b = mciso.isosurface(res,isolevel,ploc,psize,pprop)
             print('  mciso:',time.clock() - stime,'sec')
             stime = time.clock()
             
@@ -187,27 +197,68 @@ def isosurf(context):
             #obsurf.show_name = True
 
             bm = bmesh.new()
-
+            #print("   new bmesh")
             bm.from_mesh(mesurf)
+            #print("   from_mesh bmesh")
             bm.clear()
+            #print("   clear bmesh")
             sh = bm.verts.layers.shape.new("IsoSurf_mb")
+            #print("   shape bmesh")
+            p1 = bm.loops.layers.uv.new("IsoSurf_prop1")
+            #print("   uv1 bmesh")
+            p2 = bm.loops.layers.uv.new("IsoSurf_prop2")
+            #print("   uv2 bmesh")
+            lprop = 3 * 6
             for i in range(int(len(a)/9)):
 
                 vertex1 = bm.verts.new( (a[i*9], a[i*9+1], a[i*9+2]) )
                 vertex2 = bm.verts.new( (a[i*9+3], a[i*9+4], a[i*9+5]) )
                 vertex3 = bm.verts.new( (a[i*9+6], a[i*9+7], a[i*9+8]) )
                 
-                vertex1[sh] = (a[i*9] + b[i*9], a[i*9+1] + b[i*9+1], a[i*9+2] + b[i*9+2])
-                vertex2[sh] = (a[i*9+3] + b[i*9+3], a[i*9+4] + b[i*9+4], a[i*9+5] + b[i*9+5])
-                vertex3[sh] = (a[i*9+6] + b[i*9+6], a[i*9+7] + b[i*9+7], a[i*9+8] + b[i*9+8])
+                vertex1[sh] = (a[i*9] + b[i*lprop], a[i*9+1] + b[i*lprop+1], a[i*9+2] + b[i*lprop+2])
+                vertex2[sh] = (a[i*9+3] + b[i*lprop+6], a[i*9+4] + b[i*lprop+7], a[i*9+5] + b[i*lprop+8])
+                vertex3[sh] = (a[i*9+6] + b[i*lprop+12], a[i*9+7] + b[i*lprop+13], a[i*9+8] + b[i*lprop+14])
                 
+                #TEST---
+                #vertex1[sh] = (a[i*9]-b[i*lprop+3],a[i*9+1]-b[i*lprop+4],a[i*9+2]-b[i*lprop+5])
+                #vertex2[sh] = (a[i*9+3]-b[i*lprop+9], a[i*9+4]-b[i*lprop+10],a[i*9+5]-b[i*lprop+11])
+                #vertex3[sh] = (a[i*9+6]-b[i*lprop+15],a[i*9+7]-b[i*lprop+16],a[i*9+8]-b[i*lprop+17])
+                
+                #vertex1[sh] = (b[i*lprop+3],b[i*lprop+4],b[i*lprop+5])
+                #vertex2[sh] = (b[i*lprop+9], b[i*lprop+10],b[i*lprop+11])
+                #vertex3[sh] = (b[i*lprop+15],b[i*lprop+16],b[i*lprop+17])
+                #---TEST
                 bm.faces.new( (vertex1, vertex2, vertex3) ).smooth = True
+                
             #print(preview)
+            #bm.verts.ensure_lookup_table()
+            #print("   verts lookuptable bmesh")
+            bm.faces.ensure_lookup_table()
+            #print("   face lookuptable bmesh")
+            for i in range(int(len(a)/9)):
+                bm.faces[i].loops[0][p1].uv = (b[i*lprop+3],b[i*lprop+4])
+                bm.faces[i].loops[0][p2].uv = (b[i*lprop+5],0.0)
+                bm.faces[i].loops[1][p1].uv = (b[i*lprop+9], b[i*lprop+10])
+                bm.faces[i].loops[1][p2].uv = (b[i*lprop+11], 0.0)
+                bm.faces[i].loops[2][p1].uv = (b[i*lprop+15],b[i*lprop+16])
+                bm.faces[i].loops[2][p2].uv = (b[i*lprop+17],0.0)
+                #bm.faces[i].loops[0][p1].uv = (a[i*9]-b[i*lprop+3],a[i*9+1]-b[i*lprop+4])
+                #bm.faces[i].loops[0][p2].uv = (a[i*9+2]-b[i*lprop+5],0.0)
+                #bm.faces[i].loops[1][p1].uv = (a[i*9+3]-b[i*lprop+9], a[i*9+4]-b[i*lprop+10])
+                #bm.faces[i].loops[1][p2].uv = (a[i*9+5]-b[i*lprop+11], 0.0)
+                #bm.faces[i].loops[2][p1].uv = (a[i*9+6]-b[i*lprop+15],a[i*9+7]-b[i*lprop+16])
+                #bm.faces[i].loops[2][p2].uv = (a[i*9+8]-b[i*lprop+17],0.0)
+            #print("   uv loop bmesh")
             if preview == False:
                 #print(preview)
                 bmesh.ops.remove_doubles(bm,verts=bm.verts,dist=res/100)
-
+            #print("   remove doubles bmesh")
             bm.to_mesh(mesurf)
+            #print("   to_mesh bmesh")
+            bm.free()
+            #print("   free bmesh")
+            scn.update()
+            #print("   scene update")            
             mesurf.shape_keys.animation_data_clear()
             mesurf.shape_keys.key_blocks['IsoSurf_mb'].slider_min = -1.0
             mesurf.shape_keys.key_blocks['IsoSurf_mb'].value = 1/scn.render.fps
@@ -218,8 +269,7 @@ def isosurf(context):
             mesurf.shape_keys.key_blocks['IsoSurf_mb'].keyframe_insert("value",frame=scn.frame_current)
             
             
-            scn.update()
-            bm.free()
+            
             print('  Bmesh:',time.clock() - stime,'sec')
 
             #bpy.context.scene.objects.unlink(obsurf)
@@ -248,6 +298,8 @@ class UIListPanelExample(Panel):
         if 'IsoSurfer' in obj:
             layout = self.layout
             box = layout.box()
+            row = box.row()
+            row.operator("object.iso_local_uv",icon = 'GROUP_UVS',text = "Set Local UV at this time")
             row = box.row()
             row.prop(obj,"IsoSurf_res",text = "Voxel size:")
             row = box.row()
@@ -339,6 +391,94 @@ class IsoSurf(bpy.types.PropertyGroup):
     sizem = FloatProperty()
     weight = FloatProperty()    
 
+class IsoLocalUV(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.iso_local_uv"
+    bl_label = "Iso local UV"
+
+    def execute(self, context):
+        SurfList = []
+        scene = context.scene
+        print("Transfert UV start")
+        for item in context.object.IsoSurf:
+            if item.active == True:
+                if item.obj != '':
+                    if item.psys != '':
+                        SurfList.append([item.obj,item.psys])
+        for obj,psys in SurfList:
+            if bpy.data.objects[obj].data.uv_layers.active == None:
+                print("   ... local UV transfert from:",obj,psys)
+                bpy.data.objects[obj].particle_systems[psys].settings['IsoLocalUV'] = [0.0] * len(bpy.data.objects[obj].particle_systems[psys].particles) * 3
+                bpy.data.objects[obj].particle_systems[psys].particles.foreach_get("location",bpy.data.objects[obj].particle_systems[psys].settings['IsoLocalUV'])
+            else:
+            
+                object = bpy.data.objects[obj]
+                print('   ...start bake uv from:',obj,psys)
+                
+                obdata = object.data.copy()
+                object2 = bpy.data.objects.new(name="mol_uv_temp",object_data = obdata)
+                object2.matrix_world = object.matrix_world
+                
+                context.scene.objects.link(object2)
+                mod = object2.modifiers.new("tri_for_uv","TRIANGULATE")
+                mod.quad_method = "BEAUTY"
+                mod.ngon_method = "BEAUTY"
+                newmesh = object2.to_mesh(bpy.context.scene,True,"RENDER",True,False)
+                object2.data = newmesh
+                context.scene.update()
+                """
+                oldmesh = object.data
+                newmesh = object.data.copy()
+                object.data = newmesh
+                mod = object.modifiers.new("tri_for_uv","TRIANGULATE")
+                mod.use_beauty = False
+                bpy.ops.object.modifier_apply(apply_as='DATA', modifier=mod.name)
+                """
+                #print('-------------start------------')
+                attr = []
+                for par in object.particle_systems[psys].particles:
+                    parloc = (par.location * object2.matrix_world) - object2.location
+                    point = object2.closest_point_on_mesh(parloc)
+                    #print('closest:',par.location,point[0],point[2])
+                    vindex1 = object2.data.polygons[point[2]].vertices[0]
+                    vindex2 = object2.data.polygons[point[2]].vertices[1]
+                    vindex3 = object2.data.polygons[point[2]].vertices[2]
+                    v1 = (object2.matrix_world * object2.data.vertices[vindex1].co).to_tuple()
+                    v2 = (object2.matrix_world * object2.data.vertices[vindex2].co).to_tuple()
+                    v3 = (object2.matrix_world * object2.data.vertices[vindex3].co).to_tuple()
+                    uvindex1 = object2.data.polygons[point[2]].loop_start + 0
+                    uvindex2 = object2.data.polygons[point[2]].loop_start + 1
+                    uvindex3 = object2.data.polygons[point[2]].loop_start + 2
+                    uv1 = object2.data.uv_layers.active.data[uvindex1].uv.to_3d()
+                    uv2 = object2.data.uv_layers.active.data[uvindex2].uv.to_3d()
+                    uv3 = object2.data.uv_layers.active.data[uvindex3].uv.to_3d()
+                    #print(vertices1.co,vertices2.co,vertices3.co)
+                    #print(uv1,uv2,uv3)
+                    p = object2.matrix_world * point[0]
+                    v1 = Vector(v1)
+                    v2 = Vector(v2)
+                    v3 = Vector(v3)
+                    uv1 = Vector(uv1)
+                    uv2 = Vector(uv2)
+                    uv3 = Vector(uv3)
+                    #print(a,b,c,uv1,uv2,uv3,p)
+                    newuv = barycentric(p,v1,v2,v3,uv1,uv2,uv3)
+                    #print('New UVs:',newuv)
+                    parloc = par.location * object2.matrix_world
+                    dist = (Vector((parloc[0] - p[0],parloc[1] - p[1],parloc[2] - p[2]))).length
+                    newuv[2] = dist
+                    newuv = newuv.to_tuple()
+                    attr.append(newuv[0])
+                    attr.append(newuv[1])
+                    attr.append(newuv[2])
+                    #par.angular_velocity = newuv
+                object.particle_systems[psys].settings['IsoLocalUV'] = attr
+                scene.objects.unlink(object2)
+                bpy.data.objects.remove(object2)
+                bpy.data.meshes.remove(newmesh)      
+        
+        print("Transfert UV end")
+        return {'FINISHED'}
 
 def register():
     bpy.utils.register_class(OBJECT_OT_add_isosurf)
@@ -350,7 +490,7 @@ def register():
     bpy.types.Object.IsoSurf_res = FloatProperty()
     bpy.types.Object.IsoSurf_preview = BoolProperty()
     bpy.types.Scene.IsoSurf_context = StringProperty(default = "WINDOW")
-    pass
+    #bpy.utils.register_class(IsoLocalUV)
 
 
 def unregister():
@@ -359,15 +499,19 @@ def unregister():
     bpy.types.INFO_MT_mesh_add.remove(add_isosurf_button)
     bpy.utils.unregister_module(__name__)
     del bpy.types.Object.IsoSurf
-    pass
+    #bpy.utils.register_class(IsoLocalUV)
 
-
+    
 if isosurf not in bpy.app.handlers.frame_change_post:
     print('create isosurfer handlers...')
     bpy.app.handlers.persistent(isosurf_frame)
     bpy.app.handlers.frame_change_post.append(isosurf_frame)
     bpy.app.handlers.persistent(isosurf_prerender)
+    #bpy.app.handlers.render_init.append(isosurf_prerender)
     bpy.app.handlers.render_pre.append(isosurf_prerender)
     bpy.app.handlers.persistent(isosurf_postrender)
     bpy.app.handlers.render_post.append(isosurf_postrender)
+    bpy.app.handlers.render_cancel.append(isosurf_postrender)
+    bpy.app.handlers.render_complete.append(isosurf_postrender)
     print('isosurfer handler created successfully!')
+
